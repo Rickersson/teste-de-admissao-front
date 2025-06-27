276d4713fdaba14630c3fd6a12d67cfe4e55efae
Rickersson <henrique.senacnig@gmail.com>
2025-06-27 18:17:25 -0300

    refactor: mudança na identação do código

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@
 
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { UsersListComponent } from './users-list/users-list.component';
 import { EditUserComponent } from './edit-user/edit-user.component';
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,48 +1,29 @@
+
+
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { AuthService } from 'src/app/services/auth.service';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss'],
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
   hidePassword = true;
   loginForm: FormGroup;
-  error: string | null = null;
 
-  constructor(
-    private fb: FormBuilder,
-    private authService: AuthService,
-    private router: Router
-  ) {
+  constructor(private fb: FormBuilder) {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', Validators.required],
-      rememberMe: [false],
+      rememberMe: [false]
     });
   }
 
   onSubmit(): void {
     if (this.loginForm.valid) {
-      const { email, password } = this.loginForm.value;
-
-      this.authService.login(email, password).subscribe(
-        (res: any) => {
-          if (res.token) {
-            localStorage.setItem('token', res.token);
-            this.router.navigate(['/dashboard']);
-          } else {
-            this.error = 'Token não retornado.';
-          }
-        },
-        (err) => {
-          this.error = 'Credenciais inválidas ou erro no servidor.';
-          console.error(err);
-        }
-      );
+      console.log('Login data:', this.loginForm.value);
+      // Aqui você faria a chamada à API de autenticação
     }
   }
 }
